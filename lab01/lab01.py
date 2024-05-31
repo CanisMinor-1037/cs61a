e2e7551a5fa6d28c8defa6ae95facebcc7236b60
canisminor <canisminor2023@outlook.com>
2024-05-31 10:47:17 +0800

    20240531

--- lab01/lab01.py
+++ lab01/lab01.py
@@ -10,10 +10,7 @@ def falling(n, k):
     >>> falling(4, 0)
     1
     """
-    total, stop = 1, n-k
-    while n > stop:
-        total, n = total*n, n-1
-    return total
+    "*** YOUR CODE HERE ***"
 
 
 def divisible_by_k(n, k):
@@ -36,14 +33,7 @@ def divisible_by_k(n, k):
     >>> c
     0
     """
-    count = 0
-    i = 1
-    while i <= n:
-        if i % k == 0:
-            print(i)
-            count += 1
-        i += 1
-    return count
+    "*** YOUR CODE HERE ***"
 
 
 def sum_digits(y):
@@ -59,10 +49,7 @@ def sum_digits(y):
     >>> a
     6
     """
-    total = 0
-    while y > 0:
-        total, y = total + y % 10, y // 10
-    return total
+    "*** YOUR CODE HERE ***"
 
 
 def double_eights(n):
@@ -80,24 +67,5 @@ def double_eights(n):
     >>> double_eights(80808080)
     False
     """
-    prev_eight = False
-    while n > 0:
-        last_digit = n % 10
-        if last_digit == 8 and prev_eight:
-            return True
-        elif last_digit == 8:
-            prev_eight = True
-        else:
-            prev_eight = False
-        n = n // 10
-    return False
-
-
-# Alternate solution
-def double_eights_alt(n):
-    while n:
-        if n % 10 == 8 and n // 10 % 10 == 8:
-            return True
-        n //= 10
-    return False
+    "*** YOUR CODE HERE ***"
 
